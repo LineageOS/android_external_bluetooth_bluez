516008a425bc9bfc01125fcaab24c21ddfcfb97a
Archana Ramachandran <archanar@codeaurora.org>
2011-10-23 07:06:02 +0200

    Bluetooth: Fix for checking pairing state of the cached device on BT power on
    If the device type is LE then check if LE keys are present instead
    of link keys and update the paired state accordingly.
    
    Change-Id: I27412e68a6cedfc46d96051d2ec7b362fba47faf

--- src/device.c
+++ src/device.c
@@ -1095,8 +1095,20 @@ struct btd_device *device_create(DBusConnection *conn,
 
 	device->auth = 0xff;
 
-	if (read_link_key(&src, &device->bdaddr, NULL, NULL) == 0)
-		device->paired = TRUE;
+	if (type == DEVICE_TYPE_LE) {
+		DBG("Device type is LE - checking if the device is paired");
+		if (read_le_key(&src, &device->bdaddr, NULL, NULL, NULL, NULL,
+			NULL, NULL, NULL, NULL) == 0) {
+			device->paired = TRUE;
+			DBG("%s", "device->paired = TRUE");
+		}
+	} else {
+		DBG("Device type is not LE - checking if the device is paired");
+		if (read_link_key(&src, &device->bdaddr, NULL, NULL) == 0) {
+			device->paired = TRUE;
+			DBG("%s", "device->paired = TRUE");
+		}
+	}
 
 	return btd_device_ref(device);
 }
